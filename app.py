076b6b975e5866402e2f1ee36def47ac5d2fb050
fox-sys <borisberestov@ya.ru>
2021-10-13 19:01:46 +0300

    добавлеение должности на работе

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from driver import BaseDriver
 with open('result.txt', 'w') as file:
     file.close()
 
-DAY = 'c%5Bbday%5D={day}&'
-MONTH = 'c%5Bbmonth%5D={month}&'
-LINK = 'https://vk.com/search?{day}{month}c%5Bcompany%5D={company}&c%5Bname%5D=1&c%5Bper_page%5D=40&c%5Bphoto%5D=0&c%5Bsection%5D=people&c%5Bsex%5D={sex}'
+DAY_TEMPLATE = 'c%5Bbday%5D={day}&'
+MONTH_TEMPLATE = 'c%5Bbmonth%5D={month}&'
+LINK_TEMPLATE = 'https://vk.com/search?{day}{month}c%5Bcompany%5D={company}&c%5Bname%5D=1&c%5Bper_page%5D=40&c%5Bphoto%5D=0&{position}c%5Bsection%5D=people&c%5Bsex%5D={sex}'
+POSITION_TEMPLATE = 'c%5Bposition%5D={position}&'
 
 
 def login(driver, login_vk, password_vk):
@@ -17,16 +18,7 @@ def login(driver, login_vk, password_vk):
     elements_css = {
         'login': 'input[id="index_email"]',
         'login_pass': 'input[id="index_pass"]',
-        'login_button': 'button[id="index_login_button"]',
-        'message_input': 'div[id="im_editable{peer_id}"]',
-        'message_send_button': 'button.im-chat-input--send',
-        'message_text': 'div.im-mess--text',
-        'account_messages': 'li.im-mess_out',
-        'all_messages': 'li.im-mess',
-        'msg_error': 'div.error',
-        'top_profile_link': '#top_profile_link',
-        'box_body': '.box_body',
-        'limit_error': 'div.im-page--error--visible'
+        'login_button': 'button[id="index_login_button"]'
     }
     driver.get('https://vk.com')
     driver.input_text_slow(elements_css['login'], login_vk)
@@ -51,24 +43,24 @@ def split_link(link, amount):
         if amount <= 1000:
             file.write(link)
         elif 1000 < amount <= 2000:
-            [file.write(LINK.format(day='', month='', sex=i, company=company) + '\n') for i in range(1, 3)]
+            [file.write(LINK_TEMPLATE.format(day='', month='', sex=i, company=company) + '\n') for i in range(1, 3)]
         elif 2000 < amount <= 12000:
-            [file.write(LINK.format(day='', month=MONTH.format(month=i), sex=0, company=company) + '\n') for i in
+            [file.write(LINK_TEMPLATE.format(day='', month=MONTH_TEMPLATE.format(month=i), sex=0, company=company) + '\n') for i in
              range(1, 13)]
         elif 12000 < amount <= 30000:
-            [file.write(LINK.format(day=DAY.format(day=i), month='', sex=0, company=company) + '\n') for i in
+            [file.write(LINK_TEMPLATE.format(day=DAY_TEMPLATE.format(day=i), month='', sex=0, company=company) + '\n') for i in
              range(1, 32)]
         elif 30000 < amount <= 60000:
             for i in range(1, 32):
                 for j in 1, 2:
-                    file.write(LINK.format(day=DAY.format(day=i), month='', sex=j, company=company) + '\n')
+                    file.write(LINK_TEMPLATE.format(day=DAY_TEMPLATE.format(day=i), month='', sex=j, company=company) + '\n')
         else:
             for i in range(1, 32):
                 for j in range(1, 13):
                     for x in 1, 2:
-                        file.write(LINK.format(
-                            day=DAY.format(day=i),
-                            month=MONTH.format(month=j),
+                        file.write(LINK_TEMPLATE.format(
+                            day=DAY_TEMPLATE.format(day=i),
+                            month=MONTH_TEMPLATE.format(month=j),
                             sex=x,
                             company=company) + '\n')
         file.close()
@@ -78,6 +70,9 @@ if __name__ == '__main__':
     login_vk = input('Введите логин от вк: ')
     password_vk = input('Введите пароль от вк: ')
     company = input('Введите место работы: ')
-    link = LINK.format(day='', month='', sex=0, company=company)
+    position = input('Введите должность: ')
+    if position != '':
+        position = POSITION_TEMPLATE.format(position=position)
+    link = LINK_TEMPLATE.format(day='', month='', sex=0, company=company, position=position)
     info = check_link(link, login_vk, password_vk)
     split_link(*info)
